31205ec7ed9fb4b8c7e005566a83650bc925f70c
grainmad <2458898755@qq.com>
2025-06-05 19:23:41 +0800

    kv server

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,10 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"time"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -11,6 +14,8 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	key      string
+	clientID string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +24,41 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
+	lk := &Lock{
+		ck:       ck,
+		key:      l,
+		clientID: kvtest.RandValue(8),
+	}
 	// You may add code here
+
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	// 确保对应的值是空的，才可以修改值，如果网络原因导致不确定修改是否成功，再查询一次
+	for {
+		if val, ver, err := lk.ck.Get(lk.key); err == rpc.ErrNoKey || val == "" { // 锁释放
+			if err := lk.ck.Put(lk.key, lk.clientID, ver); err == rpc.OK {
+				// 锁获取成功
+				return
+			} else if err == rpc.ErrMaybe { // 锁可能获取成功, 需要验证
+				if val, _, err := lk.ck.Get(lk.key); err == rpc.OK && val == lk.clientID {
+					// 锁获取成功
+					return
+				}
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	// 确保对应的值是自己的客户端id，才可以修改值为空
+	if val, ver, err := lk.ck.Get(lk.key); err == rpc.OK && val == lk.clientID {
+		// 键已经存在，已确保其他客户端没有put的情况下，返回值只有OK和ErrMaybe
+		// ErrMaybe， 版本对应不上导致的，由于只有当前客户端先将值置空才能成功改变版本，所以返回ErrMaybe，锁必定是释放成功的
+		lk.ck.Put(lk.key, "", ver)
+	}
 }
--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -6,7 +6,7 @@ import (
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
 const Debug = false
@@ -18,15 +18,22 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type ValueVersion struct {
+	Value   string
+	Version rpc.Tversion
+}
 
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	kvmap map[string]ValueVersion
 }
 
 func MakeKVServer() *KVServer {
-	kv := &KVServer{}
+	kv := &KVServer{
+		kvmap: make(map[string]ValueVersion),
+	}
 	// Your code here.
 	return kv
 }
@@ -35,6 +42,18 @@ func MakeKVServer() *KVServer {
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	if valueVersion, ok := kv.kvmap[args.Key]; ok {
+		reply.Value = valueVersion.Value
+		reply.Version = valueVersion.Version
+		reply.Err = rpc.OK
+	} else {
+		reply.Err = rpc.ErrNoKey
+	}
+	if Debug {
+		DPrintf("Get: %s, value: %s, version: %d, err: %s", args.Key, reply.Value, reply.Version, reply.Err)
+	}
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -43,12 +62,31 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	if valueVersion, ok := kv.kvmap[args.Key]; ok { // 存在key
+		if args.Version == valueVersion.Version { // 版本匹配
+			kv.kvmap[args.Key] = ValueVersion{Value: args.Value, Version: args.Version + 1}
+			reply.Err = rpc.OK
+		} else { // 版本不匹配
+			reply.Err = rpc.ErrVersion
+		}
+	} else { // 不存在key
+		if args.Version == 0 { // 版本为0，安装新值
+			kv.kvmap[args.Key] = ValueVersion{Value: args.Value, Version: args.Version + 1}
+			reply.Err = rpc.OK
+		} else { // 版本不为0，返回错误
+			reply.Err = rpc.ErrNoKey
+		}
+	}
+	if Debug {
+		DPrintf("Put: %s, value: %s, version: %d, err: %s", args.Key, args.Value, args.Version, reply.Err)
+	}
 }
 
 // You can ignore Kill() for this lab
 func (kv *KVServer) Kill() {
 }
-
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []tester.IService {
